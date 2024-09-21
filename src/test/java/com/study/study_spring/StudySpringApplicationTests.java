e8f40d04d532b18e9a398a6581f5eaf59d46071a
yeeahji <yeeahjilee@gmail.com>
2024-09-21 14:35:32 +0900

    initial commit

---
+++ src/test/java/com/study/study_spring/StudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.study_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
