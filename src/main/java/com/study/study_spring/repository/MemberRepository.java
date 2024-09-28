0702dd95b47982f54d465e2f6fee5efb16d6bd04
yeeahji <yeeahjilee@gmail.com>
2024-09-28 17:25:04 +0900

    commit

---
+++ src/main/java/com/study/study_spring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.study.study_spring.repository;
+
+
+import com.study.study_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
